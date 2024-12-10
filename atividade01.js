8f0b2458255355a277dcaf6e5d3443d926eadca1
Ellison <ellison.m@escolar.ifrn.edu.br>
2024-12-09 21:58:44 -0300

    atv e aulas

---
+++ atividade01.js
@@ -0,0 +1,7 @@
+//declare uma vareavel para armazenar o nome do aluno, sua idade e
+//sua nota final. exiba os valores no console
+let nome = "Ellison Manoel"
+let idade = 21
+let notafinal = 9.5
+
+console.log("Nome: ", nome, "Idade:" , idade,  "Nota Final: ", notafinal)
